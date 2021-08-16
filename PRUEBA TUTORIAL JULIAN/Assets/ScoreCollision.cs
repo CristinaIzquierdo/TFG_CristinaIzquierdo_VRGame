ba8b3b5a4e4b75e4e4914741659b3e6dd490d70a
Cristina Izquierdo <62721902+CristinaIzquierdo2020@users.noreply.github.com>
2021-08-16 17:29:25 +0200

    Juego bolos y baloncesto OK

--- PRUEBA TUTORIAL JULIAN/Assets/ScoreCollision.cs
+++ PRUEBA TUTORIAL JULIAN/Assets/ScoreCollision.cs
@@ -4,21 +4,9 @@ using UnityEngine;
 
 public class ScoreCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnCollisionEnter(Collision collision)
    {
-     if(collision.transform.name == "SueloBolo")
+     if(collision.transform.name == "ColisionBolos")
      {
         bolosScore.Score += 5;
      }
