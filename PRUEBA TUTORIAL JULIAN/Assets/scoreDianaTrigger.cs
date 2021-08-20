f562e02e9531820f2d72aaca7fe5f98dd3aea63b
Cristina Izquierdo <62721902+CristinaIzquierdo2020@users.noreply.github.com>
2021-08-20 11:52:30 +0200

    Juego diana hecho

--- PRUEBA TUTORIAL JULIAN/Assets/scoreDianaTrigger.cs
+++ PRUEBA TUTORIAL JULIAN/Assets/scoreDianaTrigger.cs
@@ -4,75 +4,58 @@ using UnityEngine;
 
 public class scoreDianaTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnCollisionEnter(Collision collision)
     {
       
-       if(collision.transform.name == "PuntosBlanco")
+       if(collision.transform.name == "targetRojo")
       {
-        Debug.Log("Estoy en puntosBlanco ");
-          puntosDiana.Score += 0;
+       
+          dianaScore.Score += 1;
           //collision.gameObject.transform.children = gameObject.transform;
 
           //permite que cuando la bola choque con puntosCentro, se quede pegada
           GetComponent<Rigidbody>().isKinematic = true;
 
-      } else
-      if(collision.transform.name == "PuntosMorado")
-      {
-          puntosDiana.Score += 10;
-          //collision.gameObject.transform.children = gameObject.transform;
-
-          //permite que cuando la bola choque con puntosCentro, se quede pegada
-          GetComponent<Rigidbody>().isKinematic = true;
-
-      } else
-        if(collision.transform.name == "PuntosVerde")
-      {
-          puntosDiana.Score += 20;
-          //collision.gameObject.transform.children = gameObject.transform;
-
-          //permite que cuando la bola choque con puntosCentro, se quede pegada
-          GetComponent<Rigidbody>().isKinematic = true;
-
-      }  else  
-      if(collision.transform.name == "PuntosCentro")
-      {
-          puntosDiana.Score += 30;
-          //permite que cuando la bola choque con puntosCentro, se quede pegada
-          GetComponent<Rigidbody>().isKinematic = true;
-      }
+      } else 
 
 
-      // int caseSwitch = 1;
-
-      // switch (caseSwitch)
-      // {
-      //     case 1:
-      //         Console.WriteLine("Case 1");
-      //         break;
-      //     case 2:
-      //         Console.WriteLine("Case 2");
-      //         break;
-      //     default:
-      //         Console.WriteLine("Default case");
-      //         break;
-      // }
-   
-     
       
-      
+       if(collision.transform.name == "targetVerde")
+      {
+       
+          dianaScore.Score += 3;
+          //collision.gameObject.transform.children = gameObject.transform;
+
+          //permite que cuando la bola choque con puntosCentro, se quede pegada
+          GetComponent<Rigidbody>().isKinematic = true;
+ 
+      } else 
+
+        if(collision.transform.name == "targetBlanco")
+      {
+       
+          dianaScore.Score += 0;
+          //collision.gameObject.transform.children = gameObject.transform;
+
+          //permite que cuando la bola choque con puntosCentro, se quede pegada
+          GetComponent<Rigidbody>().isKinematic = true;
+ 
+      } 
+      else 
+
+        if(collision.transform.name == "targetAmarillo")
+      {
+       
+          dianaScore.Score += 2;
+          //collision.gameObject.transform.children = gameObject.transform;
+
+          //permite que cuando la bola choque con puntosCentro, se quede pegada
+          GetComponent<Rigidbody>().isKinematic = true;
+ 
+      } 
+
+
+
       
     }
 }
