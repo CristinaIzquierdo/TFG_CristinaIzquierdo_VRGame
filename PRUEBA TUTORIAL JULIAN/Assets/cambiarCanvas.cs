83bdb8a472e8cdf5c350db340272fd28f91907c9
Cristina Izquierdo <62721902+CristinaIzquierdo2020@users.noreply.github.com>
2021-08-29 11:33:28 +0200

    pequeños cambios en el menu

---
+++ PRUEBA TUTORIAL JULIAN/Assets/cambiarCanvas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambiarCanvas : MonoBehaviour
+{
+
+public GameObject cambiarMalo;
+public GameObject canvasBueno;
+
+   public void btn_changeCanvas()
+   {
+       cambiarMalo.gameObject.SetActive(false);
+       canvasBueno.gameObject.SetActive(true);
+
+   }
+}
